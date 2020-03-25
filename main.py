d6a85e26e5aed6911b8e61c3f5d3df49ebe207ae
wliebert <w.liebert@bsgroup.com>
2020-03-25 16:26:57 +0100

    add argparse

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from datetime import datetime
+import argparse
 
+from logic.calendarer import get_current_day_string, str_to_date
 from logic.worklogs.worklog_manager import ScrumWorklogManager
 
-ScrumWorklogManager().fill_missing_scrum_for_day()
+parser = argparse.ArgumentParser(description='Fill tempo with daily. Daily.')
+parser.add_argument('--day', type=str, nargs=1, default=get_current_day_string(),
+                    help='an anchor date (default: today), YYYY-MM-DD')
+parser.add_argument('--action', choices=['day', 'week', 'week-until-today'], default='day',
+                    help='choose which action you want to perform (default: fill for today)')
 
-y = 0
+args = parser.parse_args()
+worklog_manager = ScrumWorklogManager()
+day = str_to_date(args.day)
+if args.action == 'day':
+    worklog_manager.fill_missing_scrum_for_day(day)
+elif args.action == 'week':
+    worklog_manager.fill_missing_scrum_for_week(day)
+elif args.action == 'week-until-today':
+    worklog_manager.fill_missing_scrum_for_week_until_day(day)
